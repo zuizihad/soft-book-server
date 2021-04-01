b120a9d3585365823d604e6bf650f508a3a80efc
zuizihad <ahmedxihad@gmail.com>
2021-04-01 15:23:32 +0600

    with out order page everything almost done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
@@ -18,11 +19,11 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     const collection = client.db("softBookDB").collection("books");
+    const OrderCollection = client.db("softBookDB").collection("orders");
     console.log('db connected')
     app.get('/books', (req, res) => {
         collection.find()
             .toArray((err, items) => {
-                console.log(items)
                 res.send(items)
             })
     })
@@ -30,10 +31,40 @@ client.connect(err => {
         const newBook = req.body;
         collection.insertOne(newBook)
             .then(result => {
-                console.log('inserted count:', result.insertedCount)
                 res.send(result.insertedCount > 0)
             })
-        console.log(newBook)
+
+    })
+
+    app.delete('deleteBook/:id', (req, res) => {
+        const id = ObjectID(req.params.id);
+        collection.findOneAndDelete({ _id: id })
+            .then(documents => res.send(documents.value))
+
+    })
+
+    app.get('/book/:id', (req, res) => {
+        const id = ObjectID(req.params.id);
+        collection.find({ _id: id })
+            .toArray((err, documents) => {
+                res.send(documents[0])
+            })
+    })
+
+    app.post('/checkout', (req, res) => {
+        const newCheckout = req.body;
+        OrderCollection.insertOne(newCheckout)
+            .then((result) => {
+                res.status(200).send('inserted')
+                console.log('checkout:', newCheckout);
+            })
+    })
+
+    app.get('/orders', (req, res) => {
+        OrderCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
     })
 });
 
